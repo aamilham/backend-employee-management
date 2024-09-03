9f9cb092a47924d8330af87f7919cefc572ad166
aamilham <aamilham13@gmail.com>
2024-09-03 19:42:48 +0700

    change guard to api

--- employee-management/config/auth.php
+++ employee-management/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -42,7 +42,8 @@ return [
         ],
         'api' => [
             'driver' => 'jwt',
-            'provider' => 'users'
+            'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
